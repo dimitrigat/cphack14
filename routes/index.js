d5c4cf988e3674d111bb30343edd7ba42fc5f71e
Dimitri Gatowski <dimitri.gatowski@commerce-plus.com>
2014-07-05 15:02:27 +0200

    added products from sphere to index page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,30 @@
 module.exports = function(app) {
 
-    // Home/main
-    app.get('/', function(req, res) {
-        res.render('index', { title: 'cphack' })
-    })
+    //products
+    var products = [{name: 'Shirt', price: 12.99}, {name: 'Hose', price: 29.99}]
+
+    SphereClient = require('sphere-node-client');
+
+    var client = new SphereClient({config: {
+        'client_id' : 'JrwhqXpdIr1NJOBRIIfmW1dD',
+        'client_secret' : 'Dygrpv-Az7vrWxKwzOZq26isKZpW1wBF',
+        'project_key' : 'cphack'
+    }});
+
+    client.products.all().fetch()
+        .then(function(result) {
+            var products = [];
+            //console.log("Results: %d", result.body.total);
+            for (var i = 0; i < result.body.results.length; i++) {
+                products.push(result.body.results[i].masterData.current);
+                console.log("%j", result.body.results[i].masterData.current);
+            }
+
+            // Home/main
+            app.get('/', function(req, res) {
+                res.render('index', { title: 'C+ Hackathlon shop', products: products })
+            })
+        })
+        .fail(function(error) {console.log(error.message)});
 
 }
